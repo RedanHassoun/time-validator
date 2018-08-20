551b1cecdd89dcca91f9f084e71bd707961f7b4a
RedanHassoun <redan.has.1991@gmail.com>
2018-08-21 00:31:12 +0300

    Changed the month and day

--- timevalidator.js
+++ timevalidator.js
@@ -3,7 +3,7 @@ function checkFormat(input) {
         return false 
     } 
 
-    const regExp = /^\d{4}\-([0-1]*[0-9])\-([0-3]*[0-9])\T([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?\Z?$/
+    const regExp = /^\d{4}\-(1[0-2]|0*[1-9])\-(3[01]|0*[0-9]|[12][0-9])\T([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?\Z?$/
     
     return regExp.test(input);
 }
@@ -13,9 +13,9 @@ function checkFormat(input) {
 let strArray = ['2015-4-22T16:18:32.123Z',
                 '2015-1-12T01:02:45.786Z',
                 '2015-1-12T01:222:45.786Z', 
-                '1994-11-05T13:15:30Z',
+                '1994-4-31T00:59:59Z',
                 '1999-789-12T16:13:55.44Z',
-                null]
+                undefined]
 
 for(let i=0;i<strArray.length;i++){
     console.log(`${strArray[i]} is : ${checkFormat(strArray[i])}`)
